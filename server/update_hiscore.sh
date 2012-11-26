3a87eed6de4dc0749ee70286b8f773cd7d31b23e
Flying Spaghetti Monster <fake@example.com>
2012-11-25 18:09:07 -0800

    hi score updater

---
+++ server/update_hiscore.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /var/lib/adom/server
+
+update()
+{
+    /usr/games/adom-$1-bin -S
+
+    if ! cmp hiscore.doc /var/www/adom_hiscore/hiscore_$2.txt
+    then
+        echo "Hiscore for $2 CHANGED."
+        mv hiscore.doc /var/www/adom_hiscore/hiscore_$2.txt
+    else
+        echo "Hiscore for $2 not changed."
+    fi
+}
+
+while true
+do
+    update 100 v100
+    update 111 v111
+    update etr eternium_man
+    update swp swapgame
+    update lea leagues
+
+    echo "Hiscore updated."
+
+    sleep 60
+done
