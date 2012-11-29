e3d73bd450a40369c69f2ed7a796d5a4e2bcc207
Alucard <saturos@gmail.com>
2012-11-29 14:42:17 -0500

    add quota support

--- server/adom_adduser.sh
+++ server/adom_adduser.sh
@@ -2,9 +2,17 @@
 
 CONF=/var/lib/adom/server/adom_adduser.conf
 
+SOFT=393216
+HARD=786432 
+
 sudo adduser --conf $CONF --ingroup adomusers "$1" 
+sudo setquota -u "$1" "$SOFT" "$HARD" 0 0 /
 
 if [ $? == "0" ]; then
+        echo ""
+        echo "NOTE: You have a $SOFT KB disk limit"
+        echo ""
+        echo ""
 	echo "Your account was created. Please open a new SSH connection"
 	echo "and log in with your new username and password."
 else
