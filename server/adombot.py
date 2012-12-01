50da0dfe51e84e2e31562fa9026d5acb150f1247
Alucard <saturos@gmail.com>
2012-11-30 20:43:51 -0500

    twitter support

--- server/adombot.py
+++ server/adombot.py
@@ -4,12 +4,34 @@ import sys
 import time
 from pyinotify import WatchManager, ThreadedNotifier, ProcessEvent, IN_CLOSE_WRITE
 import os
+from tweepy import OAuthHandler, API, TweepError, debug
+import ConfigParser
+import re
 
-MIN_IRC_ANC = 200
+#irclib.DEBUG = True
+
+MIN_IRC_ANC = 2000
+MIN_TWIT_ANC = 8000
 
 FILE111 = "/var/lib/adom/public_html/adom_hiscore/hiscore_v111.txt"
 FILE100 = "/var/lib/adom/public_html/adom_hiscore/hiscore_v100.txt"
 FILEETR = "/var/lib/adom/public_html/adom_hiscore/hiscore_vetr.txt"
+
+config = ConfigParser.RawConfigParser()
+config.read('/var/lib/adom/etc/config')
+
+CONSUMER_KEY = config.get("twitter", "CONSUMER_KEY")
+CONSUMER_SECRET = config.get("twitter", "CONSUMER_SECRET")
+ACCESS_KEY = config.get("twitter", "ACCESS_KEY")
+ACCESS_SECRET = config.get("twitter", "ACCESS_SECRET")
+
+#try to set up twitter
+try:
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = API(auth)
+except TweepError as e:
+    print "Error: could not set up OAuth. {0}".format(e)
 
 def signal_handler(signal, frame):
     print "Received signal {0}".format(signal)
@@ -19,6 +41,53 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGTERM, signal_handler) 
 signal.signal(signal.SIGINT, signal_handler)
+
+def tweet(version, text):
+    m = re.match('(.*?)\. (L\d{0,1}.*?) \((M|F)\)\. \d+ xps\. \d+ turns?\. (.*?)\. (Rank: #\d{0,3}), score (\d+)',text)
+
+    raceclass = m.group(2)
+    score = int(m.group(6))
+    if score <= MIN_TWIT_ANC:
+        return
+
+    mapping = [ ('drakish ', 'Dr'), ('dwarven ','Dw'), ('dark elven ', 'De'), ('gray elven ', 'Ge'), ('high elven ', 'He'), ('gnomish ','Gn'), ('human ','Hm'), ('hurthling ', 'Hr'), ('orcish ','Or'), ('trollish ','Tr'), ('archer','Ar'), ('assassin','As'), ('barbarian','Bb'), ('bard','Br'), ('beastfighter','Bf'), ('druid','Dr'), ('elementalist','El'), ('farmer','Fa'), ('fighter','Fi'), ('healer','He'), ('merchant','Me'), ('mindcrafter','Mi'), ('monk','Mo'), ('necromancer','Ne'), ('paladin','Pa'), ('priest','Pr'), ('ranger','Ra'), ('thief','Th'), ('weaponsmith','We'), ('wizard','Wz') ]
+
+    for k, v in mapping:
+        raceclass = raceclass.replace(k, v)
+
+    reason =  m.group(4)
+
+    if " while saving h" in reason:
+        reason = reason[0:reason.find(" while saving h")]
+    elif " without even stopping to save h" in reason:
+        reason = reason[0:reason.find(" without even stopping to save h")]
+
+    if "He was " in reason:
+        reason = reason[7:]
+    elif "She was " in reason:
+        reason = reason[8:]
+    elif "He " in reason:
+        reason = reason[2:]
+    elif "She " in reason:
+        reason = reason[3:]
+
+
+    newtext = "#ADOM {0} score: {1}, {2}, {3}. {4}".format(version, m.group(1), raceclass, reason, m.group(5))
+    if newtext.__len__() > 140:
+        makeup = newtext.__len__() - 140
+        rdiff = reason.__len__() - makeup - 2
+        if rdiff < 1:
+            return
+        reason = reason[0:rdiff] + ".."
+
+    newtext = "#ADOM {0} score: {1}, {2}, {3}. {4}".format(version, m.group(1), raceclass, reason, m.group(5))
+    print newtext
+    try:
+        api.update_status(newtext)
+    except TweepError as e:
+        print "Error sending tweet. {0}".format(e)
+
+
 
 def poll_hiscore():
     global hiscore_100
@@ -40,15 +109,17 @@ def poll_hiscore():
     if c.is_connected() == True:
         for key in diff_100:
             print hiscore_100[key] + " Version 1.0.0."
-            c.privmsg(target, hiscore_100[key] + " Version 1.0.0.")
+            c.privmsg(target, "\x02New high score\x02: " + hiscore_100[key] + " Version 1.0.0.")
+            tweet("1.0.0", hiscore_100[key]);
             
         for key in diff_111:
             print hiscore_111[key] + " Version 1.1.1."
-            c.privmsg(target, hiscore_111[key] + " Version 1.1.1.")
+            c.privmsg(target, "\x02New high score\x02: " + hiscore_111[key] + " Version 1.1.1.")
+            tweet("1.1.1", hiscore_111[key]);
 
         for key in diff_etr:
             print hiscore_etr[key] + " Played The Eternium Man challenge."
-            c.privmsg(target, hiscore_etr[key] + " Played The Eternium Man challenge.")
+            c.privmsg(target, "\x02New high score\x02: " + hiscore_etr[key] + " Played The Eternium Man challenge.")
 
 def import_hiscore(file):
     f = open(file, "r")
