2c883a6f986592bf0b1dd39beb940a8d3b3db68a
Alucard <saturos@gmail.com>
2012-12-02 18:47:32 -0500

    error checking & info note

--- server/adom_adduser.sh
+++ server/adom_adduser.sh
@@ -5,16 +5,16 @@ CONF=/var/lib/adom/server/adom_adduser.conf
 SOFT=393216
 HARD=786432 
 
-sudo adduser --conf $CONF --ingroup adomusers "$1" 
-sudo setquota -u "$1" "$SOFT" "$HARD" 0 0 /
+echo -e "\033[1mYou MUST set a password, but all information after that is OPTIONAL.\033[0m"
 
-if [ $? == "0" ]; then
-        echo ""
-        echo "NOTE: You have a $SOFT KB disk limit"
-        echo ""
-        echo ""
-	echo "Your account was created. Please open a new SSH connection"
-	echo "and log in with your new username and password."
-else
-	echo "There was an error. Please try again."
-fi
+sudo adduser --conf $CONF --ingroup adomusers "$1" 
+if [ $? != "0" ]; then echo "There was an error. Please try again."; sleep 5; exit 1; fi
+sudo setquota -u "$1" "$SOFT" "$HARD" 0 0 /
+if [ $? != "0" ]; then echo "There was an error. Please try again."; sleep 5; exit 1; fi
+
+echo ""
+echo "NOTE: You have a $SOFT KB disk limit"
+echo ""
+echo ""
+echo "Your account was created. Please open a new SSH connection"
+echo "and log in with your new username and password."
