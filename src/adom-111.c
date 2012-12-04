9863e2108e4885894bb69398e977e52918eec946
Alucard <saturos@gmail.com>
2012-12-03 22:54:00 -0500

    add explanatory comments

--- src/adom-111.c
+++ src/adom-111.c
@@ -32,6 +32,12 @@
 //  1000 seems to be about 4 actions.
 #define COUNT_BEF_ANC 1000
 
+/* 
+  Each level is specified by two codes, stored right next to each other.
+  To find others, attach to ADOM with GDB, and print the address of LEVELID
+  below; this is the first value.  Then add 4 to LEVELID and print it; this
+  address is the second value.
+*/
 #define WILDERNESS 0x04
 #define WILDERNESS_2 0x01
 #define SMC_1 0x1C
@@ -49,6 +55,13 @@
 #define BDCBOT_1 0x28
 #define BDCBOT_2 0x02
 
+/* 
+  LEVELID is where the code for the current level is stores in memory.
+  To find it yourself, start ADOM, then start a memory trainer (such as
+  scanmem).  Search for '4' for the wilderness, then go to the small cave
+  and search for 28. There's usually only one result.  This address is the one
+  that should go in LEVELID. (If you need another, Terinyo is 5).
+*/
 #ifdef ADOM_111
   #define LEVELID 0x082add1c
 #elif defined ADOM_100
