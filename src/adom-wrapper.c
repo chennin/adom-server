77753cd7234a674e5f5bcc49bef95dba1a358346
Alucard <saturos@gmail.com>
2012-12-05 10:41:06 -0500

    reindent all and remove tabs

--- src/adom-wrapper.c
+++ src/adom-wrapper.c
@@ -86,21 +86,21 @@ int is_fatal_sig(int sig) {
 
   switch(sig) {
 
-  case SIGTRAP:
-  case SIGWINCH:
+    case SIGTRAP:
+    case SIGWINCH:
 
-    return 0;
+      return 0;
 
-  default:
-    snprintf(buf, 512, "/tmp/fatal_sig_UID%u", getuid());
-    f = fopen(buf, "w");
-    
-    if(f) {
-      fprintf(f, "Received fatal signal %d\n", sig);
-      fclose(f);
-    }
+    default:
+      snprintf(buf, 512, "/tmp/fatal_sig_UID%u", getuid());
+      f = fopen(buf, "w");
 
-    return 1;
+      if(f) {
+        fprintf(f, "Received fatal signal %d\n", sig);
+        fclose(f);
+      }
+
+      return 1;
   }
 }
 
@@ -128,11 +128,11 @@ int main(int argc, char **argv)
   e = asprintf(&SAGESO, "%s%s", BINLOC, "adom-sage-jaakkos.so");
   f = asprintf(&SAGEPATH, "%s%s", BINLOC, "adom-sage");
 
-  #ifndef LEAGUE
+#ifndef LEAGUE
   g = asprintf(&ADOMBIN, "%s%s", BINLOC, "adom-111-bin");
-  #else
+#else
   g = asprintf(&ADOMBIN, "%s%s", BINLOC, "adom-lea-bin");
-  #endif
+#endif
 #elif defined ADOM_100
   g = asprintf(&ADOMBIN, "%s%s", BINLOC, "adom-100-bin");
 #elif defined ADOM_120p3
@@ -157,7 +157,7 @@ int main(int argc, char **argv)
   }
 
   if(argc > 1 && !strcmp(argv[1], "--enable-sage"))
-     sage = 1;
+    sage = 1;
 
   sigemptyset(&mask);
   sigaddset(&mask, SIGINT);
@@ -171,107 +171,107 @@ int main(int argc, char **argv)
   srand(time(NULL));
 
   switch(pid = fork()) {
-    
-  case -1:
-    perror("fork");
-    break;
-    
-  case 0: /*  child process */
-    ptrace(PTRACE_TRACEME, 0, 0, 0);
 
-    if(!sage) {
-      execl(ADOMBIN, ADOMBIN, NULL);
-    }
+    case -1:
+      perror("fork");
+      break;
 
-    else {
-      execl(SAGEPATH, SAGEPATH, "-a", ADOMBIN, "-s", SAGESO, NULL);
-    }
+    case 0: /*  child process */
+      ptrace(PTRACE_TRACEME, 0, 0, 0);
 
-    break;
-    
-  default: /* parent process */
-    wait(&wait_val);
-    handle_sig(pid, wait_val);
-  
-    do {
-      ptrace(PTRACE_SYSCALL, pid, 0, 0);
-      wait(&wait_val);
-
-      handle_sig(pid, wait_val);
-      orig_eax = ptrace(PTRACE_PEEKUSER, pid, 4*ORIG_EAX, NULL);
-    } while(orig_eax != SYS_time);
-    
-    int counter = 0;
-    char *desc = NULL;
-
-    while(1) {
-      if(ptrace(PTRACE_SYSCALL, pid, 0, 0) == -1) {
-
+      if(!sage) {
+        execl(ADOMBIN, ADOMBIN, NULL);
       }
-      
-      wait(&wait_val);
-      handle_sig(pid, wait_val);
-      
-      if(WIFSTOPPED(wait_val) && !is_fatal_sig(WSTOPSIG(wait_val))) {
-	ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-	
-	int curloc_v1 = 0, curloc_v2 = 0;
-        getdata(pid, LEVELID, (char*)&curloc_v1, 1);
-        getdata(pid, LEVELID+4, (char*)&curloc_v2, 1);
-	long cur_turn = 0;	
-	getdata(pid, TURNCOUNTER, (char*)&cur_turn, 1);
 
-	//set desc if this is a level we should announce
-	//Comparing the loc in this manner ..
-	if (cur_turn > prev_turn && curloc_v1 == prevloc_v1 && curloc_v2 == prevloc_v2) {
-          if (prevloc_v1 == TF4_1 && curloc_v2 == TF4_2) { desc = "top of the Tower of Eternal Flames"; }
-	  #ifdef SMC_TEST
-          else if (prevloc_v1 == SMC_1 && curloc_v2 == SMC_2) { desc = "Small Cave"; }
-	  #endif
-  	  else if (prevloc_v1 == D50_1 && curloc_v2 == D50_2) { desc = "D:50"; }
-  	  else if (prevloc_v1 == MANATEMP_1 && curloc_v2 == MANATEMP_2) { desc = "Mana Temple"; }
-	  else if (prevloc_v1 == BDCBOT_1 && curloc_v2 == BDCBOT_2) { desc = "the bottom of the Blue Dragon Caves"; }
-	  else { counter = 0; desc = NULL; } // unset if if it's not
-	}
-
-	// if desc is set, announce it
-	if (desc != NULL) {
-	  // use shared memory?
-	  struct stat locfinfo;
-	  counter++;
-	  char fname[1024];
-	  FILE *tmpf;
-	  snprintf(fname, 1024, "%s/%s", STATUSDIR_PATH, me);
-
-          time_t now = time(0);
-	  time_t mtime = 0;
-
-	  if (stat(fname, &locfinfo) >= 0) {
-	    mtime = locfinfo.st_mtim.tv_sec;
-	  }
-	  if (now > mtime + SEC_BET_ANC) {
-	    tmpf = fopen(fname, "w");
-	    if(tmpf) {
-	      fprintf(tmpf, "%s", desc);
-	      fclose(tmpf);
-	      counter = 0;
-	    }
-	  }
-        }
-	prev_turn = cur_turn;
-	prevloc_v1 = curloc_v1;
-	prevloc_v2 = curloc_v2;
-      }
-      else if (WIFSTOPPED(wait_val)) {
-	ptrace(PTRACE_CONT, pid, 0, WSTOPSIG(wait_val));
-      }
       else {
-	kill(pid, 9);
-	return return_wrapper(1);
+        execl(SAGEPATH, SAGEPATH, "-a", ADOMBIN, "-s", SAGESO, NULL);
       }
-      
-    }
+
+      break;
+
+    default: /* parent process */
+      wait(&wait_val);
+      handle_sig(pid, wait_val);
+
+      do {
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
+        wait(&wait_val);
+
+        handle_sig(pid, wait_val);
+        orig_eax = ptrace(PTRACE_PEEKUSER, pid, 4*ORIG_EAX, NULL);
+      } while(orig_eax != SYS_time);
+
+      int counter = 0;
+      char *desc = NULL;
+
+      while(1) {
+        if(ptrace(PTRACE_SYSCALL, pid, 0, 0) == -1) {
+
+        }
+
+        wait(&wait_val);
+        handle_sig(pid, wait_val);
+
+        if(WIFSTOPPED(wait_val) && !is_fatal_sig(WSTOPSIG(wait_val))) {
+          ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+
+          int curloc_v1 = 0, curloc_v2 = 0;
+          getdata(pid, LEVELID, (char*)&curloc_v1, 1);
+          getdata(pid, LEVELID+4, (char*)&curloc_v2, 1);
+          long cur_turn = 0;	
+          getdata(pid, TURNCOUNTER, (char*)&cur_turn, 1);
+
+          //set desc if this is a level we should announce
+          //Comparing the loc in this manner ..
+          if (cur_turn > prev_turn && curloc_v1 == prevloc_v1 && curloc_v2 == prevloc_v2) {
+            if (prevloc_v1 == TF4_1 && curloc_v2 == TF4_2) { desc = "top of the Tower of Eternal Flames"; }
+#ifdef SMC_TEST
+            else if (prevloc_v1 == SMC_1 && curloc_v2 == SMC_2) { desc = "Small Cave"; }
+#endif
+            else if (prevloc_v1 == D50_1 && curloc_v2 == D50_2) { desc = "D:50"; }
+            else if (prevloc_v1 == MANATEMP_1 && curloc_v2 == MANATEMP_2) { desc = "Mana Temple"; }
+            else if (prevloc_v1 == BDCBOT_1 && curloc_v2 == BDCBOT_2) { desc = "the bottom of the Blue Dragon Caves"; }
+            else { counter = 0; desc = NULL; } // unset if if it's not
+          }
+
+          // if desc is set, announce it
+          if (desc != NULL) {
+            // use shared memory?
+            struct stat locfinfo;
+            counter++;
+            char fname[1024];
+            FILE *tmpf;
+            snprintf(fname, 1024, "%s/%s", STATUSDIR_PATH, me);
+
+            time_t now = time(0);
+            time_t mtime = 0;
+
+            if (stat(fname, &locfinfo) >= 0) {
+              mtime = locfinfo.st_mtim.tv_sec;
+            }
+            if (now > mtime + SEC_BET_ANC) {
+              tmpf = fopen(fname, "w");
+              if(tmpf) {
+                fprintf(tmpf, "%s", desc);
+                fclose(tmpf);
+                counter = 0;
+              }
+            }
+          }
+          prev_turn = cur_turn;
+          prevloc_v1 = curloc_v1;
+          prevloc_v2 = curloc_v2;
+        }
+        else if (WIFSTOPPED(wait_val)) {
+          ptrace(PTRACE_CONT, pid, 0, WSTOPSIG(wait_val));
+        }
+        else {
+          kill(pid, 9);
+          return return_wrapper(1);
+        }
+
+      }
   }
-  
+
   return return_wrapper(0);
 }
