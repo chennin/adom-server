f1e6506c8299bc66e16d29c2030574d0ace1dd48
Alucard <saturos@gmail.com>
2012-12-07 16:13:51 -0500

    Graceful registration; fix IP tracking; deny excessive regs

--- server/registration_check.py
+++ server/registration_check.py
@@ -1,33 +1,37 @@
 import sys, os
 
-f = open("/var/lib/adom/tmp/registered_ips")
+cur_ip = os.environ["SSH_CONNECTION"].split()[0]
 
-lines = f.readlines()
+if os.path.exists("/var/lib/adom/tmp/registered_ips"):
+  f = open("/var/lib/adom/tmp/registered_ips", 'r')
 
-if len(lines) > 100:
+  lines = f.readlines()
+
+  if len(lines) > 100:
     print "Sorry, too many users have registered today!"
     print "Please try again tomorrow. Thank you for your interest!"
     sys.exit(1)
 
-cur_ip = os.environ["SSH_CONNECTION"].split()[0]
-occurrences = 0
+  occurrences = 0
 
-for ip in lines:
+  for ip in lines:
     if ip.strip() == cur_ip:
         occurrences += 1
 
-#if occurrences >= 3:
-#    print "Sorry, but there have already been 3 registrations"
-#    print "from your IP-address. Please try again tomorrow."
-#    print "Thank you for your interest!"
-#    sys.exit(1)
+  if occurrences >= 3:
+    print "Sorry, but there have already been 3 registrations"
+    print "from your IP address. Please try again tomorrow."
+    print "Thank you for your interest!"
+    sys.exit(1)
 
-f.close()
+  f.close()
 
 if len(sys.argv) == 2 and sys.argv[1] == "--checkonly":
     sys.exit(0)
 
 else:
-    os.system("echo \"" + cur_ip + "\" >> /var/lib/adom/tmp/registered_ips")
+  f = open("/var/lib/adom/tmp/registered_ips", 'a')
+  f.write(cur_ip + "\n")
+  f.close()
 
 sys.exit(0)
