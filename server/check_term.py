6ff4ba2ec6bcc86340cee9cdc3dc78218d85ac1f
Alucard <saturos@gmail.com>
2012-12-08 15:33:11 -0500

    remove reference to old server

--- server/check_term.py
+++ server/check_term.py
@@ -16,7 +16,7 @@ else:
 
 if sz[0] != 25 or sz[1] != 80:
     if silent == False:
-        print "Your terminal size appears to be " + str(sz[1]) + "x" + str(sz[0]) + " characters. A 80x25 -sized terminal is REQUIRED for playing ADOM on this server. Please correct your terminal settings and try again. You may want to consult the readme, available at the main menu, and at http://ancardia.ath.cx/adom_server_info.txt"
+        print "Your terminal size appears to be " + str(sz[1]) + "x" + str(sz[0]) + " characters. A 80x25 -sized terminal is REQUIRED for playing ADOM on this server. Please correct your terminal settings and try again. You may want to consult the readme, available at the main menu, and on the web site."
 
     sys.exit(1)
 
