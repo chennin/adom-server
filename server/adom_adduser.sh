3ba4627a7b45911c4a7368bdbf8907c0c2d16751
Alucard <saturos@gmail.com>
2012-12-07 21:38:51 -0500

    Ask for less (but different) information

--- server/adom_adduser.sh
+++ server/adom_adduser.sh
@@ -5,16 +5,17 @@ CONF=/var/lib/adom/server/adom_adduser.conf
 SOFT=393216
 HARD=786432 
 
-echo -e "\033[1mYou MUST set a password, but all information after that is OPTIONAL.\033[0m"
+echo -e "\033[1mYou MUST set a password, but your e-mail is OPTIONAL.\033[0m"
+echo "If you provide an e-mail address, password recovery will be possible."
 
-sudo adduser --conf $CONF --ingroup adomusers "$1" 
+read -e -p "Enter email address (optional): " email
+
+sudo adduser --conf $CONF --gecos "${email}" --ingroup adomusers "$1" 
 if [ $? != "0" ]; then echo "There was an error. Please try again."; sleep 5; exit 1; fi
 sudo setquota -u "$1" "$SOFT" "$HARD" 0 0 /
-if [ $? != "0" ]; then echo "There was an error. Please try again."; sleep 5; exit 1; fi
 
 echo ""
 echo "NOTE: You have a $SOFT KB disk limit"
-echo ""
 echo ""
 echo "Your account was created. Please open a new SSH connection"
 echo "and log in with your new username and password."
