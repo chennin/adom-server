91ec27a5f29c078f38305b8dd19565f5849b559c
Alucard <saturos@gmail.com>
2012-12-08 16:00:45 -0500

    remove unused code; fix spurious termsize warning

--- server/ttyrecord.py
+++ server/ttyrecord.py
@@ -9,7 +9,5 @@ if len(ps_lines) == 0:
 	tstr = "-".join(time.ctime(time.time()).split())
 	os.system("termrec -f ttyrec \"" + recdir + "/ttyrec-" + tstr + "\"")
 	sys.exit(1)
-	# os.system("/var/lib/adom/server/adom_wrapper|dosrecorder \"" + recdir + "/" + tstr + ".dm2\"");
-	# os.system("kill `pgrep -u " + str(os.getuid()) + " adom_wrapper`")
 
 sys.exit(0)
