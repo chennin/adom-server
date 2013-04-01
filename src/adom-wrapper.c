0b85ff513e7247380594c98a5bc866ff7cb30954
Christopher Henning <saturos@gmail.com>
2013-04-01 14:20:36 -0400

    Bump Sage for p13

--- src/adom-wrapper.c
+++ src/adom-wrapper.c
@@ -219,8 +219,8 @@ int main(int argc, char **argv)
   memset(sagesoname,'\0',SOLEN);
   memset(sagename,'\0',SAGELEN);
 
-  strncpy(sagesoname, "adom-sage-0.9.8.so", SOLEN);
-  strncpy(sagename, "adom-sage-0.9.8", SAGELEN);
+  strncpy(sagesoname, "adom-sage-0.9.9.so", SOLEN);
+  strncpy(sagename, "adom-sage-0.9.9", SAGELEN);
 
   snprintf(binname, BINLEN, "adom-%s-bin", vers);
   if (strcmp(vers,"111") == 0) {
