95d79c06ef9fdaaffd8c53010954a70bdb91e080
Christopher Henning <saturos@gmail.com>
2013-09-29 11:59:56 -0400

    Implement stat tracking and displaying.

---
+++ public_html/stats.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+use strict; use warnings;
+use CGI;  # Change to "use CGI::Compress::Gzip;" if you have this module and want to compress output
+use POSIX qw(strftime);
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);  # uncomment to spit errors at the user instead of the Web server log
+use Config::Simple;
+use DBI;
+                                                                                                                                                             my $cfg = new Config::Simple("/var/lib/adom/etc/config") or die "Unable to open config file $!\n";
+
+my $WATER = 1; my $FIRE = 2; my $AIR = 4; my $EARTH = 8; my $MANA = 16; my $FISTY = 32; my $ANDY = 64;
+
+my %races = ( 'Dr'=>    'drakish', 'Dw'=>    'dwarven', 'De'=>    'dark elven', 'Ge'=>    'gray elven', 'He'=>    'high elven', 'Gn'=>    'gnomish', 'Hm'=>    'human', 'Hr'=>    'hurthling', 'Or'=>    'orcish', 'Tr'=>    'trollish', 'Rt'=>    'ratling', 'Me'=>    'mist elven',);
+my %classes = ( 'Ar'=>    'archer', 'As'=>    'assassin', 'Bb'=>    'barbarian', 'Br'=>    'bard', 'Bf'=>    'beastfighter', 'Dr'=>    'druid', 'El'=>    'elementalist', 'Fa'=>    'farmer', 'Fi'=>    'fighter', 'He'=>    'healer', 'Me'=>    'merchant', 'Mi'=>    'mindcrafter', 'Mo'=>    'monk', 'Ne'=>    'necromancer', 'Pa'=>    'paladin', 'Pr'=>    'priest', 'Ra'=>    'ranger', 'Th'=>    'thief', 'We'=>    'weaponsmith', 'Wz'=>    'wizard', 'Ck'=>    'chaos knight', 'Du'=>    'duelist',);
+
+my $cgi = new CGI; # Change to "... new CGI::Compress::Gzip;" if you have this module
+print $cgi->header; # HTTP header
+print $cgi->start_html(
+		-title => "ADOM Server User Stats",
+		-script => { -type =>'JAVASCRIPT', -src => "sorttable.js", },
+		-style => { -src => 'k.css'},
+		);
+my $user = undef;
+$user = $cgi->param('user');
+if (defined($user)) { # display user stats
+        my $dbh = DBI->connect("dbi:Pg:dbname=" . $cfg->param('DB_NAME') . ";host=" . $cfg->param('SQL_HOST') . ";", $cfg->param('SQL_USER'), $cfg->param('SQL_PASS'));
+	if (!defined $dbh) { die "Cannot connect to database! $!\n"; }
+	my $sth = $dbh->prepare(" SELECT * from stats WHERE username=? ORDER BY date ") or die $dbh->errstr;
+	$sth->execute($user) or die $dbh->errstr;
+
+	print "<h2>Stats for $user</h2>\n";
+        print "<p>Click any table header to sort.</p>\n";
+	print "<table class='sortable'><thead><tr>\n";
+	my @headers = ("Name", "Score", "Lvl", "R /", "C", "Align", "Game Time", "Turns", "Result", "Bosses", "Date");
+	foreach my $header (@headers) {
+		print "<th>$header</th>";
+	}
+	print "</tr></thead><tbody>\n";
+
+	while (my $row = $sth->fetchrow_hashref()) {
+		print "<tr>";
+		print "<td class='left'>$row->{'name'}</td>";
+		print "<td class='left'>$row->{'score'}</td>";
+		print "<td>$row->{'level'}</td>";
+		print "<td><span class='tooltip' title='$races{$row->{'race'}}'>$row->{'race'}</span></td>";
+		print "<td><span class='tooltip' title='$classes{$row->{'class'}}'>$row->{'class'}</span></td>";
+		print "<td>$row->{'align'}</td>";
+		print "<td>$row->{'time'}</td>";
+		print "<td>$row->{'turns'}</td>";
+		print "<td><a href='/adom_users/$user/flgs/$row->{'filename'}'>$row->{'reason'}</a></td>";
+                my $bossstr = "";
+                if ($row->{'bs'} & $WATER) { $bossstr .= '<span title="Snake from Beyond">s</span>'; }
+                if ($row->{'bs'} & $FIRE) { $bossstr .= '<span title="Ancient Chaos Wyrm">W</span>'; }
+                if ($row->{'bs'} & $AIR) { $bossstr .= '<span title="Master Summoner">@</span>'; }
+                if ($row->{'bs'} & $EARTH) { $bossstr .= '<span title="Ancient Stone Beast">F</span>'; }
+                if ($row->{'bs'} & $MANA) { $bossstr .= '<span title="Chaos Archmage">@</span>'; }
+                if ($row->{'bs'} & $FISTY) { $bossstr .= '<span title="Fistanarius">&</span>'; }
+                if ($row->{'bs'} & $ANDY) { $bossstr .= '<span title="ElDeR cHaOs GoD">@</span>'; }
+		print "<td>$bossstr</td>";
+		print "<td sorttable_customkey='$row->{'date'}'>" . strftime "%d %b '%y", localtime $row->{'date'} . "</td>";
+		print "</tr>\n";
+	}
+
+	print "</tbody></table>\n";
+
+	$sth->finish;
+	$dbh->disconnect;
+}
+else { # no user, so ask for one
+	print '<form name="input" action="stats.pl"><strong>Enter username:</strong><input type="text" name="user"><input type="submit" value="Submit"></form>';
+}
+
+print $cgi->end_html;
+
---
+++ server/record-flg.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use POSIX qw(getpwuid);
+use File::Basename;
+use File::stat;
+use Config::Simple;
+use DBI;
+
+my $cfg = new Config::Simple("/var/lib/adom/etc/config") or die "Unable to open config file $!\n";
+my $filename = $ARGV[0];
+if ($#ARGV != 0) { die "Provide a flg name"; }
+open FLG, "<$filename"or die $!;
+
+my %races = (
+    'drakish' => 'Dr', 'dwarven' => 'Dw', 'dark elven' => 'De', 'gray elven' => 'Ge', 'high elven' => 'He', 'gnomish' => 'Gn', 'human' => 'Hm', 'hurthling' => 'Hr', 'orcish' => 'Or', 'trollish' => 'Tr', 'ratling' => 'Rt', 'mist elven' => 'Me',);
+my $racereg = join("|", map {quotemeta} keys %races);
+my %classes = (
+    'archer' => 'Ar', 'assassin' => 'As', 'barbarian' => 'Bb', 'bard' => 'Br', 'beastfighter' => 'Bf', 'druid' => 'Dr', 'elementalist' => 'El', 'farmer' => 'Fa', 'fighter' => 'Fi', 'healer' => 'He', 'merchant' => 'Me', 'mindcrafter' => 'Mi', 'monk' => 'Mo', 'necromancer' => 'Ne', 'paladin' => 'Pa', 'priest' => 'Pr', 'ranger' => 'Ra', 'thief' => 'Th', 'weaponsmith' => 'We', 'wizard' => 'Wz', 'chaos knight' => 'Ck', 'duelist' => 'Du',);
+my $classreg = join("|", map {quotemeta} keys %classes);
+
+my $WATER = 1; my $FIRE = 2; my $AIR = 4; my $EARTH = 8; my $MANA = 16; my $FISTY = 32; my $ANDY = 64;
+
+my $user = getpwuid($<); # REAL_USER_ID
+my ($name, $align, $score, $level, $race, $class, $reason, $time, $turns, $date) = undef;
+my $bs = 0; # boss bitmask
+my $catlover = 0;
+while (my $line = <FLG>) {
+  if ($line =~ /(.+?)\s+St:.*? ([CNL][-=+LNC]?)$/) { # Name and stats line
+    $name = $1; $align = $2;
+    $name =~ s/^ /_/g;
+  }
+  elsif ($line =~ /e scored (-?\d+) points(?:\.| and advanced to level (\d{1,2})\.)/) { # level and score
+    $level = 1; if (defined $2) { $level = $2; }
+    $score = $1; 
+  }
+  elsif ($line =~ /e survived for/) { # Time, turns
+    $line .= <FLG>; # Always seems to be two lines
+    if ($line =~ /e survived for (\d+) years?, (\d+) days?,.(\d+).hours?,.(\d+).minutes?.and.(\d+).seconds?.\((\d+).turns?\)\./s) {
+      $time = sprintf "%dd %02d:%02d:%02d", $1*360+$2, $3, $4, $5; 
+      $turns = $6;
+    }
+  }
+  elsif ((defined($name)) && ($line =~ /^\Q$name\E, the/)) { # name, reason, r/c
+    if ($line !~ /\.$/) { $line .= <FLG>; } # not always two lines
+    $line =~ /\Q$name\E, the ([^,]+), (.+?)(?:.while.saving|.without.even|\.)/s;
+    my $raceclass = lc $1;
+    $reason = $2;
+    $raceclass =~ s/($racereg)/$races{$1}/go;
+    $raceclass =~ s/($classreg)/$classes{$1}/go;
+    $raceclass =~ /(\S+) (\S+)/;
+    ($race, $class) = ($1, $2);
+    $reason =~ s/[\n\r]/ /g;
+  }
+  # boss kill tracking
+  elsif ($line =~ /1 Snake from Beyond/) { $bs += $WATER }
+  elsif ($line =~ /1 Ancient Chaos Wyrm/) { $bs += $FIRE }
+  elsif ($line =~ /1 Master Summoner/) { $bs += $AIR }
+  elsif ($line =~ /1 Ancient Stone Beast/) { $bs += $EARTH }
+  elsif ($line =~ /1 Chaos Archmage/) { $bs += $MANA }
+  elsif ($line =~ /1 greater balor/) { $bs += $FISTY }
+  elsif ($line =~ /1 ElDeR cHaOs GoD/) { $bs += $ANDY }
+  elsif ($line =~ /adhered to the principles of the Cat Lord/) { $catlover = 1; }
+}
+close FLG;
+$date = stat($filename)->mtime; # modified time will have to do
+$filename = basename($filename); # store filename so we don't have to deal with special characyter replacement
+#print "$user, $name, $align, $score, $level, $race, $class, $reason, $time, $turns, $catlover, $bs, $date, $filename";
+
+# connect and record in SQL table
+my $dbh = DBI->connect("dbi:Pg:dbname=" . $cfg->param('DB_NAME') . ";host=" . $cfg->param('SQL_HOST') . ";", $cfg->param('SQL_USER'), $cfg->param('SQL_PASS'));
+if (!defined $dbh) {
+   die "Cannot connect to database! $!\n";
+}
+my $sth = $dbh->prepare("
+  INSERT INTO stats (username, name, align, score, level, race, class, reason, time, turns, catlover, bs, date, filename) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)") or die $dbh->errstr;
+$sth->execute($user, $name, $align, $score, $level, $race, $class, $reason, $time, $turns, $catlover, $bs, $date, $filename) or die $dbh->errstr;
+$sth->finish;
+$dbh->disconnect;
