1212106eb12d3e76b4b842dc45eb1fa89f470f3f
Christopher Henning <saturos@gmail.com>
2013-09-29 12:53:22 -0400

    Bump version of Sage used

--- src/adom-wrapper.c
+++ src/adom-wrapper.c
@@ -219,8 +219,8 @@ int main(int argc, char **argv)
   memset(sagesoname,'\0',SOLEN);
   memset(sagename,'\0',SAGELEN);
 
-  strncpy(sagesoname, "adom-sage-0.9.12.so", SOLEN);
-  strncpy(sagename, "adom-sage-0.9.12", SAGELEN);
+  strncpy(sagesoname, "adom-sage-0.9.13.so", SOLEN);
+  strncpy(sagename, "adom-sage-0.9.13", SAGELEN);
 
   snprintf(binname, BINLEN, "adom-%s-bin", vers);
   if (strcmp(vers,"111") == 0) {
