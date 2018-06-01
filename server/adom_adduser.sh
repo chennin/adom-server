8121b4c1effca7def7fd1cb7af126104931a7940
Chris Henning <saturos@gmail.com>
2018-06-01 10:07:32 -0400

    Add disallowed username check

--- server/adom_adduser.sh
+++ server/adom_adduser.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+if [ "$1" == "" ]; then
+  echo "ERROR: no user"
+  exit 2
+fi
 
 CONF=/var/lib/adom/server/adom_adduser.conf
 
@@ -8,7 +12,16 @@ HARD=786432
 echo -e "\033[1mYou MUST set a password, but your e-mail is OPTIONAL.\033[0m"
 echo "If you provide an e-mail address, password recovery will be possible."
 
-read -e -p "Enter email address (optional): " email
+email=""
+until [ ! "${email}" == "" ]; do
+  read -e -p "Enter email address (optional): " email
+  if [[ "${email}" == "" ]]; then email="invalid@example.com"; fi
+  if [[ ! "${email}" =~ ^[0-9a-zA-Z.+-]+@[0-9a-zA-Z.-]+$ ]]; then
+    echo "That email looks invalid, please try again"
+    email=""
+  fi
+done
+
 
 sudo adduser --conf $CONF --gecos ",${email},," --ingroup adomusers "$1" 
 if [ $? != "0" ]; then echo "There was an error. Please try again."; sleep 5; exit 1; fi
